deb4555f36be61b934ef7d96740f25329832a9e1
Benjamin Sundvall <benjamin@urgott.se>
2019-11-12 19:48:06 +0100

    Project setup

---
+++ Unlinked/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveDirection = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+        controller.SimpleMove(moveDirection.normalized);
+    }
+}
